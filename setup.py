16ab2bdd9fb8ec0ebd02784780193cdfa141e1de
Akkad Abraham <akkad.abraham@stud.tu-darmstadt.de>
2024-05-21 12:23:51 +0200

    Use .toml file for build system

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="BSSD_derivation_for_Lanelet2",
-    version="0.9",
-    description='Extends Lanelet2 maps with empty BSSD structure and derive behavioral demands',
-    author='Jannik Hildebrand',
-    author_email='jannik.hildebrand@t-online.de',
-    packages=find_packages(),
-    install_requires=[
-        'numpy==1.22.3',
-        'osmium==3.2.0'
-    ],
-    python_requires='>=3.8'
-
-)
+setup()
